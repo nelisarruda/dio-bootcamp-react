e5540d282f3c9701056c33b1d768533cc2e769c1
Nelis Arruda <nelisxxi@gmail.com>
2021-10-13 17:31:49 -0300

    Desafio 02(base)

---
+++ Desafios Iniciais JavaScript/02.js
@@ -0,0 +1,13 @@
+let X = parseFloat(gets());
+
+if (X>=0 && X<=25){print("Intervalo [0,25]" );
+
+}else if(X > 25 && X <=     ){print("Intervalo (25,50]");
+
+}else if(X >       && X<=     ){print("Intervalo (50,75]");
+
+}else if(          ){print("Intervalo (75,100]");        //complete o código nos espaços em branco
+
+}else{print("Fora de intervalo");
+
+}
